5d0eccdb565150b59a9a1eb4cce69a9b249ebb11
Shahriyar Mirzai <shakompk@gmail.com>
2020-05-10 16:35:07 +0400

    Updates

---
+++ precache-manifest.cb7a77c6e19801fc0ea9b8cf9cf975e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67097226cdb62cf0532419f26c5dafc8",
+    "url": "/Project-Stocks/index.html"
+  },
+  {
+    "revision": "9e832b7506e6b6d71d58",
+    "url": "/Project-Stocks/static/css/2.35fcc229.chunk.css"
+  },
+  {
+    "revision": "c116ee67580d3964d986",
+    "url": "/Project-Stocks/static/css/main.e96876cc.chunk.css"
+  },
+  {
+    "revision": "9e832b7506e6b6d71d58",
+    "url": "/Project-Stocks/static/js/2.eadd3bbe.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Project-Stocks/static/js/2.eadd3bbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c116ee67580d3964d986",
+    "url": "/Project-Stocks/static/js/main.e0014f5c.chunk.js"
+  },
+  {
+    "revision": "6e822b5f437800004b10",
+    "url": "/Project-Stocks/static/js/runtime-main.1717841c.js"
+  }
+]);
